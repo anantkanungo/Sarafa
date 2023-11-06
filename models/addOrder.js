2dd9199db926ff58c43271a34e1ae090011f6dbf
Mrinalineethakur <121418843+Mrinalineethakur@users.noreply.github.com>
2023-11-06 15:25:16 +0530

    Create addOrder.js

---
+++ models/addOrder.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const OrderShcema = new mongoose.Schema({
+
+    category: {
+        type: String,
+        required: true,
+        enum: ['ring', 'earring','bangle', 'chain', 'necklace', 'nosepin', 'pendants', 'mangalsutra', 'others'],
+        default: 'ring'
+    },
+    Shop: {
+        type: mongoose.Schema.ObjectId, 
+        ref: 'Customer'
+    },
+    image: {
+        data: Buffer,
+        contentType: String
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    quantity: {
+        type: Number,
+        min: 1
+    },
+    size: {
+        type: Number,
+
+    },
+    weight: {
+        type: Number
+    },
+    tunch: {
+        type: Number,
+        min: 1,
+        max: 100
+    }
+
+})
+
+module.exports = mongoose.model("AddOrder", OrderShcema);
