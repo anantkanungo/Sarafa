d0c50900dac226a6e9db0cfad528808941e1680f
Mrinalinee <121418843+Mrinalineethakur@users.noreply.github.com>
2023-11-13 22:32:35 +0530

    Update allRuotes.js

--- routes/allRuotes.js
+++ routes/allRuotes.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const multer = require('multer');
 const { Login } = require("../controllers/adminLogin");
 const { CreateUser } = require("../controllers/createUser");
 const { UserLogin } = require("../controllers/userLogin");
-const {GetAllOrder, PlaceOrder } = require("../controllers/order");
+const { GetAllOrder, PlaceOrder } = require("../controllers/order");
+const { authMiddleware } = require("../middleware/authMiddleware");
 const router = express.Router();
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/uploads")
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now()+ "-"+ file.originalname)
+    }
+})
+
+const upload = multer({ storage });
 
 // POST Request || admin login 
 router.post('/adminlogin', Login);
@@ -12,9 +24,8 @@ router.post('/adminlogin', Login);
 router.post('/adduser', CreateUser);
 // POST Request || user login 
 router.post('/userlogin', UserLogin);
-// POST Request || add product
-router.post('/placeorder', PlaceOrder)
-// GET Requst || get all products in admin panel
-router.get('/allorders', GetAllOrder)
-
+// POST Request || add order
+router.post('/placeorder', upload.single('design'), PlaceOrder);
+// GET Requst || get all orders in cutomer panel
+router.get('/allorders', authMiddleware, GetAllOrder);
 module.exports = router;
