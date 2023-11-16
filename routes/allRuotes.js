312f452eb6b41b7ad26ed92b44313591c19a41a7
Mrinalinee <121418843+Mrinalineethakur@users.noreply.github.com>
2023-11-16 12:55:46 +0530

    Update allRuotes.js

--- routes/allRuotes.js
+++ routes/allRuotes.js
@@ -3,8 +3,9 @@ const multer = require('multer');
 const { Login } = require("../controllers/adminLogin");
 const { CreateUser } = require("../controllers/createUser");
 const { UserLogin } = require("../controllers/userLogin");
-const { GetAllOrder, PlaceOrder } = require("../controllers/order");
+const { GetAllOrder, PlaceOrder, PendingOrders } = require("../controllers/order");
 const { authMiddleware } = require("../middleware/authMiddleware");
+const { CreateCatalog, GetCatalog } = require("../controllers/catalog");
 const router = express.Router();
 
 const storage = multer.diskStorage({
@@ -26,6 +27,14 @@ router.post('/adduser', CreateUser);
 router.post('/userlogin', UserLogin);
 // POST Request || add order
 router.post('/placeorder', upload.single('design'), PlaceOrder);
-// GET Requst || get all orders in cutomer panel
-router.get('/allorders', authMiddleware, GetAllOrder);
+// GET Request || get all orders in cutomer panel
+router.get('/allorders', GetAllOrder);
+// GET Request || get all pending orders in cutomer panel
+router.get('/pendingorders', PendingOrders);
+// POST Request || Add item into catalog in admin panel
+router.post('/addcatalog', upload.single('design'), CreateCatalog);
+// Get Request || get catalog list
+router.get('/getcatalog',GetCatalog);
+
+
 module.exports = router;
