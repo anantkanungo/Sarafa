b21f6efa7ab507b89ceff8961f22096e38e60082
Mrinalinee <121418843+Mrinalineethakur@users.noreply.github.com>
2023-12-26 14:09:33 +0530

    Delete admin directory

--- admin/src/pages/ManageUser/Distributor.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Distributor = () => {
-  return (
-    <div>Distributor</div>
-  )
-}
-
-export default Distributor
--- admin/src/pages/TaskAssign.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TaskAssign = () => {
-  return (
-    <div>TaskAssign</div>
-  )
-}
-
-export default TaskAssign
