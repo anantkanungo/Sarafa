c0af4a0b98fbf2931910840afbe893260e14c44c
Mrinalinee <121418843+Mrinalineethakur@users.noreply.github.com>
2024-02-23 17:58:09 +0530

    Update KarigerDetails.jsx

--- admin/src/pages/ManageUser/KarigerDetails.jsx
+++ admin/src/pages/ManageUser/KarigerDetails.jsx
@@ -4,16 +4,22 @@ import { AiFillDelete, AiFillEdit } from 'react-icons/ai';
 import { ToastContainer, toast } from 'react-toastify';
 import Badge from 'react-bootstrap/Badge';
 import ListGroup from 'react-bootstrap/ListGroup';
+import { useNavigate } from 'react-router-dom';
 const KarigerDetails = () => {
+  const navigate = useNavigate();
   const [list, setList] = useState([]);
+  let token = localStorage.getItem("token");
   const delItem = async (delId) => {
     console.log("delId", delId);
-    const res = await axios.delete(`http://139.59.58.151:8000/kariger/delete/${delId}`);
+    const res = await axios.delete(`http://139.59.58.151:8000/kariger/delete/${delId}`, {
+      headers: {
+          "Authorization": "Bearer " + token
+      }
+  });
     if (res.data.success) { toast.warn("deleted..."); }
   }
 
   useEffect(() => {
-    let token = localStorage.getItem("token");
     axios.get("http://139.59.58.151:8000/kariger/list", {
       headers: {
         "Authorization": "Bearer " + token
@@ -27,7 +33,7 @@ const KarigerDetails = () => {
     <>
     <div className="container mt-5" >
       <ToastContainer />
-      <div className="pt-3"><h3 className="text-dark text-center mt-4"><strong>Kariger</strong></h3>
+      <div className="pt-3"><h3 className="text-dark text-center mt-4">Karigar</h3>
         <hr /></div>
       <div className='container  ms-auto me-auto '>
         <ListGroup as="ol" numbered>
@@ -38,9 +44,12 @@ const KarigerDetails = () => {
                 <ListGroup.Item
                   as="li"
                   className="d-flex justify-content-between align-items-start  ">
-                  <div className="ms-2 me-auto">
-                    <div className="fw-bold">{items.name}</div>
-                    Address:{items.address}
+                  <div className="ms-2 me-auto" onClick={() => { navigate(`/k_order/list/${items._id}`) }}>
+                    <div className="fw-bold">Name: {items.name}</div>
+                    UserId: {items.userId}<br/>
+                    Address: {items.address}<br/>
+                    Workshop: {items.workshop.name}
+
                   </div>
                   {/* <Badge bg="secondary"  type= "button" className='me-1 cursor-pointer' pill><AiFillEdit />Edit </Badge> */}
                   <Badge bg="danger" type="button" pill  onClick={() => { delItem(items._id) }}  ><AiFillDelete />Delete </Badge>
