e98404f466a5cf3236fcf8153fb8d95969f1867f
Mrinalinee <121418843+Mrinalineethakur@users.noreply.github.com>
2024-02-23 17:54:19 +0530

    Update CreateCatMenu.jsx

--- admin/src/pages/Catalog/CreateCatMenu.jsx
+++ admin/src/pages/Catalog/CreateCatMenu.jsx
@@ -1,21 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { ToastContainer, toast } from 'react-toastify'
-import { IoMdImages } from "react-icons/io"
-const CreateCatMenu = () => {
+import Form from 'react-bootstrap/Form';
+import { IoMdImages } from "react-icons/io";
+const CreateCatalog = () => {
+    // const [selectedImages, setSelectedImages] = useState([]);
     const [image, setImage] = useState("");
-    const [category, setCategory] = useState("");
+    const [values, setValues] = useState({
+        "category": "",
+        "size": "",
+        "designCode": "",
+        "description": "",
+        "weight": "",
 
+    })
+
+    const inputHandler = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value })
+    }
+    
+    let token = localStorage.getItem("token");
+
+    const [catalog, setCatalog] = useState([]);
+  
+    useEffect(() => {
+        const fetchCatalog = async () => {
+            axios.get(`http://139.59.58.151:8000/getallcatalog/category`, {
+                headers: {
+                    "Authorization": "Bearer " + token
+                }
+            }).then((res) => {
+                console.log(res.data)
+                setCatalog(res.data.data);
+            })
+        }
+        fetchCatalog();
+    },[catalog]);
+    console.log("catalog", catalog);
     const addcatelog = async (e) => {
         e.preventDefault();
         const form_data = new FormData();
         form_data.append("catalog", image);
-        form_data.append("category", category);
+        form_data.append("category", values.category);
+        form_data.append("designCode", values.designCode);
+        form_data.append("size", values.size);
+        form_data.append("weight", values.weight);
+        form_data.append("description", values.description);
         console.log(image);
-
-        let token = localStorage.getItem("token");
+console.log("category",values.category);
         try {
-            let res = await axios.post(`http://139.59.58.151:8000/addcatalog/category`, form_data, {
+            let res = await axios.post(`http://139.59.58.151:8000/addcatalog`, form_data, {
                 headers: {
                     "Authorization": 'Bearer ' + token,
                     Accept: "application/json",
@@ -24,7 +58,7 @@ const CreateCatMenu = () => {
             });
             console.log(res.data);
             if (res.data.success) {
-                toast.success("Catalog Menu Added...");
+                toast.success("Catalog Added...");
             } else {
                 console.log("error")
                 // toast.error("There might be error...Try Again");
@@ -33,16 +67,25 @@ const CreateCatMenu = () => {
             console.error(error.response.data);
         }
         setImage("");
-        setCategory("");
+        setValues({
+            "category": "",
+            "size": "",
+            "designCode": "",
+            "description": "",
+            "weight": "",
+        })
+        setCatalog([]);
     }
-
+       
+    
+  
     return (
         <>
             <div className='container'>
                 <div className='d-flex row mx-2 mt-5 pt-5'>
                     <ToastContainer />
                     <div className='justify-content-center align-items-center ' >
-                        <div className="pt-3"><h3 className="text-dark text-center" ><strong>Add Catalog Menu</strong></h3>
+                        <div className="pt-3"><h3 className="text-dark text-center" >Add Catalogue</h3>
                             <hr />
                         </div>
                         <div className='col-sm-6 mx-auto '>
@@ -56,10 +99,32 @@ const CreateCatMenu = () => {
                                     </div>
                                 </div>
                                 <div className="form-group mb-3">
-                                    <label htmlFor="category">Category</label>
-                                    <input type="text" className="form-control" id="category" name='category' onChange={(e) => setCategory(e.target.value)} value={category} />
+                                        <Form.Select aria-label="Default select example"  name='category'  onChange={inputHandler}   >
+                                        <option value="">Select Category</option>
+                                        {catalog && catalog.map((items, index)=><option key={index} value={items.category}>{items.category}</option>
+                                    )}
+                                    </Form.Select>
+                                  
+                                    
+                                    {/* <label htmlFor="category">Category</label>
+                                    <input type="text" className="form-control" id="category" name='category' onChange={inputHandler} value={values.category} /> */}
                                 </div>
-
+                                <div className="form-group mb-3">
+                                    <label htmlFor="designCode">Design Code</label>
+                                    <input type="text" className="form-control" id="designCode" name='designCode' onChange={inputHandler} value={values.designCode} />
+                                </div>
+                                <div className="form-group mb-3">
+                                    <label htmlFor="description">Description</label>
+                                    <input type="text" className="form-control" id="description" name='description' onChange={inputHandler} value={values.description} />
+                                </div>
+                                <div className="form-group mb-3">
+                                    <label htmlFor="size">Size</label>
+                                    <input type="text" className="form-control" id="size" name='size' onChange={inputHandler} value={values.size} />
+                                </div>
+                                <div className="form-group mb-3">
+                                    <label htmlFor="weight">Weight</label>
+                                    <input type="text" className="form-control" id="weight" name='weight' onChange={inputHandler} value={values.weight} />
+                                </div>
                                 <span className='px-1 ms-auto'>
                                     <button className="btn btn-secondary m-2" type="submit" >Upload</button>
                                 </span>
@@ -68,8 +133,9 @@ const CreateCatMenu = () => {
                     </div>
                 </div>
             </div>
+
         </>
     )
 }
 
-export default CreateCatMenu
+export default CreateCatalog
