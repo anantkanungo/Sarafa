18d1495512fae2904b61c5813ff4821cec9299e0
Mrinalinee <121418843+Mrinalineethakur@users.noreply.github.com>
2024-02-23 17:53:29 +0530

    Update CatalogMenu.jsx

--- admin/src/pages/Catalog/CatalogMenu.jsx
+++ admin/src/pages/Catalog/CatalogMenu.jsx
@@ -7,9 +7,14 @@ import { useNavigate } from 'react-router-dom';
 const CatalogMenu = () => {
     const navigate = useNavigate();
     const [catalog, setCatalog] = useState([]);
+    const token = localStorage.getItem("token");
     useEffect(() => {
         const fetchCatalog = async () => {
-            axios.get(`http://139.59.58.151:8000/getallcatalog/category`).then((res) => {
+            axios.get(`http://139.59.58.151:8000/getallcatalog/category`, {
+                headers: {
+                    "Authorization": "Bearer " + token
+                }
+            }).then((res) => {
                 console.log(res.data)
                 setCatalog(res.data.data);
             })
@@ -19,25 +24,25 @@ const CatalogMenu = () => {
     return (
 
         <>
-            <div className='container pt-5 mt-5'>
-                <div className="pt-2"><h3 className="text-dark text-center mt-2"><strong>Catalog Menu</strong></h3>
+            <div className='container pt-5 mt-5 '>
+                <div className="pt-2"><h3 className="text-dark text-center mt-2">Catalogue Menu</h3>
                     <hr /></div>
-                <div className='row'>
+                <div className='row ms-auto me-auto'>
                     {catalog && catalog.map((items, index) => {
                         return (
-                            <Card className='ms-2 mt-2 text-center' style={{ width: '18rem', }}  >
-
-                                <Card.Body>
-                                    <Card.Img variant="top" className='' style={{ width: '14rem', height: '14rem' }} src={items.image} />
-                                    <div className="d-grid gap-2">
-                                        <hr/>
-                                    <Button variant="secondary" size='lg' onClick={() => { navigate(`/catalog_update/${items.category}`) }} >
-                                        {items.category}
-                                    </Button>
-                                    </div>
-                                </Card.Body>
-                            </Card>
-
+                            <div className='col-sm-6 col-md-6 col-lg-3'>
+                                <Card className='ms-2 mt-2 text-center' style={{ width: '16rem', }}  >
+                                    <Card.Img variant="top" className='p-2 mt-2 ms-auto me-auto' style={{ width: '14rem', height: '14rem' }} src={items.image} />
+                                    <Card.Body>
+                                        <div className="d-grid ">
+                                            {/* <hr /> */}
+                                            <Button variant="secondary" size='lg' onClick={() => { navigate(`/catalog_update/${items.category}`) }} >
+                                                {items.category}
+                                            </Button>
+                                        </div>
+                                    </Card.Body>
+                                </Card>
+                            </div>
                         )
                     })}
                 </div>
