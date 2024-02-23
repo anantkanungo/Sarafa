219813ef35025d5765234dd2bbe2a82523788c57
Mrinalinee <121418843+Mrinalineethakur@users.noreply.github.com>
2024-02-23 17:57:43 +0530

    Update Distributor.jsx

--- admin/src/pages/ManageUser/Distributor.jsx
+++ admin/src/pages/ManageUser/Distributor.jsx
@@ -4,17 +4,23 @@ import { AiFillDelete, AiFillEdit } from 'react-icons/ai';
 import { ToastContainer, toast } from 'react-toastify';
 import Badge from 'react-bootstrap/Badge';
 import ListGroup from 'react-bootstrap/ListGroup';
+import { useNavigate } from 'react-router-dom';
 
 const Distributor = () => {
+  const navigate = useNavigate();
   const [list, setList] = useState([]);
+  let token = localStorage.getItem("token");
   const delItem = async (delId) => {
     console.log("delId", delId);
-    const res = await axios.delete(`http://139.59.58.151:8000/distributor/delete/${delId}`);
+    const res = await axios.delete(`http://139.59.58.151:8000/distributor/delete/${delId}`, {
+      headers: {
+          "Authorization": "Bearer " + token
+      }
+  });
     if (res.data.success) { toast.warn("deleted..."); }
   }
 
   useEffect(() => {
-    let token = localStorage.getItem("token");
     axios.get("http://139.59.58.151:8000/distributor/list", {
       headers: {
         "Authorization": "Bearer " + token
@@ -28,7 +34,7 @@ const Distributor = () => {
     <>
       <div className="container mt-5" >
         <ToastContainer />
-        <div className="pt-3"><h3 className="text-dark text-center mt-4"><strong>Distributor</strong></h3>
+        <div className="pt-3"><h3 className="text-dark text-center mt-4">Distributor</h3>
           <hr /></div>
         <div className='container  ms-auto me-auto '>
           <ListGroup as="ol" numbered>
@@ -39,8 +45,11 @@ const Distributor = () => {
                   <ListGroup.Item
                     as="li"
                     className="d-flex justify-content-between align-items-start  ">
-                    <div className="ms-2 me-auto">
-                      <div className="fw-bold">{items.name}</div>
+                    <div className="ms-2 me-auto" onClick={() => { navigate(`/d_shops/list/${items.uid}`) }}>
+                      <div className="fw-bold">Name: {items.name}</div>
+                      <div className="fw-bold"></div>
+                      Code: {items.agent} <br/>
+                      UserId: {items.userId} <br/>
                       Address:{items.address}
                     </div>
                     {/* <Badge bg="secondary"  type= "button" className='me-1 cursor-pointer' pill><AiFillEdit />Edit </Badge> */}
