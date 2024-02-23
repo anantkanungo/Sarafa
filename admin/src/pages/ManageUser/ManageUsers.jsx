02489c10b4e5347dd9c8e75e0633410a2e97598d
Mrinalinee <121418843+Mrinalineethakur@users.noreply.github.com>
2024-02-23 17:58:34 +0530

    Update ManageUsers.jsx

--- admin/src/pages/ManageUser/ManageUsers.jsx
+++ admin/src/pages/ManageUser/ManageUsers.jsx
@@ -7,57 +7,48 @@ const ManageUsers = () => {
     const navigate = useNavigate();
     return (
         <>
-            <div className='container mt-5 pt-5'>
-                <div className="pt-3"><h3 className="text-dark text-center" ><strong>Manage Users</strong></h3>
-                    <hr />
-                </div>
+           <div className="container mt-5" >
+        <div className="pt-3"><h3 className="text-dark text-center mt-4">Manage Users</h3>
+          <hr /></div>
                 <div className='text-center border-bottom pb-3 pt-3'><button className='btn btn-secondary' onClick={() => navigate('/add_user')}>Add New User</button></div>
                 <div>
                     <div className='row pt-4 d-flex justify-content-evenly'>
-                        <Card style={{ width: '14rem' }}>
+                        <div className='col-sm-6 col-md-4 col-lg-3 mt-1 mb-1'>
+                        <Card  className='' style={{ width: '14rem' }}>
                             <Card.Body className='ms-auto me-auto justify-content-center'>
-                                <div className='text-center'><BiSolidUserCircle style={{ height: "60%", width: "60%", borderRadius: "50%" }} /></div>
+                                <div className='text-center'><BiSolidUserCircle className='icon' style={{ height: "60%", width: "60%", borderRadius: "50%" }} /></div>
                                 <Card.Title>Customers' List</Card.Title>
-                                {/* <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text> */}
-                                <Button variant="secondary" onClick={() => { navigate('/customer') }}>Check List</Button>
                             </Card.Body>
+                                <Button variant="secondary"  onClick={() => { navigate('/customer') }}>Check List</Button>
                         </Card>
+                        </div>
+                        <div className='col-sm-6 col-md-4 col-lg-3 mt-1 mb-1'>
                         <Card style={{ width: '14rem' }}>
                             <Card.Body className='ms-auto me-auto justify-content-center'>
-                                <div className='text-center'><BiSolidUserCircle style={{ height: "60%", width: "60%", borderRadius: "50%" }} /></div>
+                                <div className='text-center'><BiSolidUserCircle className='icon' style={{ height: "60%", width: "60%", borderRadius: "50%" }} /></div>
                                 <Card.Title>Distributors' List</Card.Title>
-                                {/* <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text> */}
+                            </Card.Body>
                                 <Button variant="secondary" onClick={() => { navigate('/distributor') }}>Check List</Button>
-                            </Card.Body>
                         </Card>
+                        </div>
+                        <div className='col-sm-6 col-md-4 col-lg-3 mt-1 mb-1'>
                         <Card style={{ width: '14rem' }}>
                             <Card.Body className='ms-auto me-auto justify-content-center'>
-                                <div className='text-center'><BiSolidUserCircle style={{ height: "60%", width: "60%", borderRadius: "50%" }} /></div>
+                                <div className='text-center'><BiSolidUserCircle className='icon' style={{ height: "60%", width: "60%", borderRadius: "50%" }} /></div>
                                 <Card.Title>WorkShops' List</Card.Title>
-                                {/* <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text> */}
-                                <Button variant="secondary" onClick={() => { navigate('/workshops') }}>Check List</Button>
                             </Card.Body>
+                                <Button variant="secondary" onClick={() => { navigate('/workshops') }}>Check List</Button>
                         </Card>
+                        </div>
+                        <div className='col-sm-6 col-md-4 col-lg-3 mt-1 mb-1'>
                         <Card style={{ width: '14rem' }}>
                             <Card.Body className='ms-auto me-auto justify-content-center'>
-                                <div className='text-center'><BiSolidUserCircle style={{ height: "60%", width: "60%", borderRadius: "50%" }} /></div>
-                                <Card.Title>  Karigers' List</Card.Title>
-                                {/* <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text> */}
-                                <Button variant="secondary" onClick={() => { navigate('/kariger') }}>Check List</Button>
+                                <div className='text-center'><BiSolidUserCircle className='icon' style={{ height: "60%", width: "60%", borderRadius: "50%" }} /></div>
+                                <Card.Title>Karigars' List</Card.Title>
                             </Card.Body>
+                                <Button variant="secondary" onClick={() => { navigate('/kariger') }}>Check List</Button>
                         </Card>
+                        </div>
                     </div>
                 </div>
             </div>
