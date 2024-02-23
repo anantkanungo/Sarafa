858288722dbb36cd191de55e20c9b939710d735a
Mrinalinee <121418843+Mrinalineethakur@users.noreply.github.com>
2024-02-23 18:02:51 +0530

    Update App.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Header from './components/Header';
-import TaskAssign from './pages/TaskAssign';
 import CatalogUpdate from './pages/Catalog/CatalogUpdate';
 import Workshops from './pages/ManageUser/Workshops';
 import KarigerDetails from './pages/ManageUser/KarigerDetails';
@@ -20,6 +19,11 @@ import CreateCatalog from './pages/Catalog/CreateCatalog';
 import Distributor from './pages/ManageUser/Distributor';
 import CatalogMenu from './pages/Catalog/CatalogMenu';
 import CreateCatMenu from './pages/Catalog/CreateCatMenu';
+import W_OrderList from './pages/ManageUser/W_OrderList';
+import K_OrderList from './pages/ManageUser/K_OrderList';
+import C_OrderList from './pages/ManageUser/C_OrderList';
+import D_ShopsList from './pages/ManageUser/D_ShopsList';
+import Notification from './pages/Notification';
 
 function App() {
   return (
@@ -29,19 +33,24 @@ function App() {
       <Route exact path='/' element={<Welcome/>} />
         <Route exact path='/login' element={<Login />} />
         <Route exact path='/home' element={<Protected Component= {Home}/>}/>
-        <Route exact path='/task_assign' element={<TaskAssign/>} />
-        <Route exact path='/catalog_create' element={<CreateCatalog />} />
-        <Route exact path='/catalog_menu' element={<CatalogMenu />} />
-        <Route exact path='/addcatalog_menu' element={<CreateCatMenu />} />
-        <Route exact path='/catalog_update/:category' element={<CatalogUpdate />} />
-        <Route exact path='/workshops' element={<Workshops />} />
-        <Route exact path='/kariger' element={<KarigerDetails />} />
-        <Route exact path='/distributor' element={<Distributor />} />
-        <Route exact path='/customer' element={<CustomerDetails />} />
-        <Route exact path='/add_user' element={<AddUser />} />
-        <Route exact path='/manage_users' element={<ManageUsers />} />
-        <Route exact path='/review_orders' element={<ReviewOrders />} />
-        <Route exact path='/editorder/:id' element={<EditOrders/>}/>
+        {/* <Route exact path='/task_assign' element={<TaskAssign/>} /> */}
+        <Route exact path='/catalog_create' element={<Protected Component= {CreateCatalog}/>} />
+        <Route exact path='/catalog_menu' element={<Protected Component= {CatalogMenu}/>} />
+        <Route exact path='/addcatalog_menu' element={<Protected Component= {CreateCatMenu}/>} />
+        <Route exact path='/catalog_update/:category' element={<Protected Component= {CatalogUpdate}/>}/>
+        <Route exact path='/workshops' element={<Protected Component= {Workshops}/>} />
+        <Route exact path='/kariger' element={<Protected Component= {KarigerDetails}/>} />
+        <Route exact path='/distributor'element={<Protected Component= {Distributor}/>} />
+        <Route exact path='/customer' element={<Protected Component= {CustomerDetails}/>} />
+        <Route exact path='/add_user' element={<Protected Component= {AddUser}/>} />
+        <Route exact path='/manage_users'element={<Protected Component= {ManageUsers}/>} />
+        <Route exact path='/review_orders' element={<Protected Component= {ReviewOrders}/>} />
+        <Route exact path='/editorder/:id' element={<Protected Component= {EditOrders}/>}/>
+        <Route exact path='/w_order/list/:id' element={<Protected Component= {W_OrderList}/>}/>
+        <Route exact path='/k_order/list/:id'element={<Protected Component= {K_OrderList}/>}/>
+        <Route exact path='/c_order/list/:id' element={<Protected Component= {C_OrderList}/>}/>
+        <Route exact path='/d_shops/list/:id' element={<Protected Component= {D_ShopsList}/>}/>
+        <Route exact path='/notification' element={<Protected Component= {Notification}/>} />
       </Routes>
     </Router>
   );
