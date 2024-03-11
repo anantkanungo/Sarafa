6e28c2261edaab9cbdcee22ebdc464e0335b0a4f
mahima9728 <mahimarawat979@gmail.com>
2024-03-11 18:36:18 +0530

    update

--- src/screens/OrderCompleted.js
+++ src/screens/OrderCompleted.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   FlatList,
   Image,
@@ -7,13 +7,14 @@ import {
   Text,
   TouchableOpacity,
   View,
+  RefreshControl,
 } from 'react-native';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
-const OrderCompleted = ({details, navigation}) => {
+const OrderCompleted = ({ details, navigation }) => {
   const [orders, setOrders] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  // const [address, setAddress] = useState('');
+  const [refreshing, setRefreshing] = useState(false);
 
   const fetchOrders = async () => {
     try {
@@ -28,25 +29,30 @@ const OrderCompleted = ({details, navigation}) => {
       );
 
       const userData = response.data.data[0];
-      // Filter orders to only include those with 'statusIs': 'completed'
       const completedOrders =
-        userData.task?.filter(order => order.statusIs === 'completed') || [];
-      setOrders(completedOrders); // Set the filtered orders
+        userData.task?.filter((order) => order.statusIs === 'completed') || [];
+      setOrders(completedOrders);
       setIsLoading(false);
     } catch (error) {
       console.log(error);
+    } finally {
+      setRefreshing(false);
     }
+  };
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    fetchOrders();
   };
 
   useEffect(() => {
     fetchOrders();
-      console.log(orders);
-  }, [orders]); // address, orders
+  }, [orders]);
 
-  const renderItem = ({item}) => (
+  const renderItem = ({ item }) => (
     <TouchableOpacity onPress={() => navigateToOrderPage(item)}>
       <View style={styles.card} key={item._id}>
-        <Image style={styles.cardImage} source={{uri: item.image[0]}} />
+        <Image style={styles.cardImage} source={{ uri: item.image[0] }} />
         <View style={styles.cardHeader}>
           <Text style={styles.title}>{item.category}</Text>
           <Text style={styles.order}>Weight: {item.weight}</Text>
@@ -57,29 +63,27 @@ const OrderCompleted = ({details, navigation}) => {
     </TouchableOpacity>
   );
 
-  const navigateToOrderPage = selectedTask => {
-    navigation.navigate('OrderPage', {selectedTask});
+  const navigateToOrderPage = (selectedTask) => {
+    navigation.navigate('OrderPage', { selectedTask });
   };
 
   return (
-    <View style={{flex: 1, marginHorizontal: 10}}>
-      {/* FlatList */}
-      {isLoading ? (
-        <Text>Loading...</Text>
-      ) : (
-        <FlatList
-          style={styles.list}
-          data={orders}
-          keyExtractor={item => item._id}
-          ItemSeparatorComponent={() => <View style={styles.separator} />}
-          renderItem={renderItem}
-        />
-      )}
+    <View style={{ flex: 1, marginHorizontal: 10 }}>
+      <FlatList
+        style={styles.list}
+        data={orders}
+        keyExtractor={(item) => item._id}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+        renderItem={renderItem}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
     </View>
   );
 };
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     loading: state.loading,
     details: state.login.details,
