682e6096bb902c83a3a80d5bdb3572d5062a7e48
Mrinalinee <121418843+Mrinalineethakur@users.noreply.github.com>
2024-04-07 17:12:24 +0530

    Delete admin/src/redux directory

--- admin/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import LoginSlice from './features/LoginSlice.js';
-export const store = configureStore({
-    reducer: {
-        login: LoginSlice
-    }
-})
-
-export default store;
